22a2ee8edc360f819d7e3b4366c125b60cd6de52
LeannaJane <leannajane2002@outlook.com>
2026-03-30 22:40:28 +0100

    feat: adding scale

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ canvas.pack(expand=True, fill="both")
 frame = tk.Frame(root)
 frame.pack(pady=10)
 
+
+size_slider = tk.Scale(
+    root, 
+    from_=1,
+    to=30,
+    orient="horizontal",
+    label="Brush size"
+)
+
+size_slider.set(3)
+size_slider.pack()
+
 def set_colour(colour):
     global current_colour
     current_colour = colour 
@@ -30,16 +42,18 @@ def draw(event):
             last_x, last_y,
             event.x, event.y,
             fill=current_colour,
-            width=3
+            width=size_slider.get()
         )
 
     last_x = event.x
     last_y = event.y 
 
+
 tk.Button(frame, text="Red", width=10, command=lambda: set_colour("red")).pack(side="left", padx=5)
 tk.Button(frame, text="Yellow", width=10, command=lambda: set_colour("yellow")).pack(side="left", padx=5)
 tk.Button(frame, text="Green", width=10, command=lambda: set_colour("green")).pack(side="left", padx=5)
 tk.Button(frame, text="Blue", width=10, command=lambda: set_colour("blue")).pack(side="left", padx=5)
+
 
 canvas.bind("<Button-1>", start_draw)
 canvas.bind("<B1-Motion>", draw)
